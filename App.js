9e93d4aa6745957fc5f87aa2439bfdd77108ae25
TaneliJarvenpaa <114722920+TaneliJarvenpaa@users.noreply.github.com>
2023-08-30 10:21:57 +0300

    Tehtävään lisätty lista laskutoimituksista joka ei tallennu

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
   const [number1, setNumber1] = useState('');
   const [number2, setNumber2] = useState('');
   const [result, setResult] = useState('');
+  const [data, setData] = useState([]);
 
   const handleNumberChange = (text, numberType) => {
     if (numberType === 1) {
@@ -19,14 +20,20 @@ export default function App() {
     const num1 = parseFloat(number1);
     const num2 = parseFloat(number2);
 
-     
+    let newResult = '';
     if (operator === '+') {
-      setResult((num1 + num2).toString());
+      newResult = (num1 + num2).toString();
     } else if (operator === '-') {
-      setResult((num1 - num2).toString());
+      newResult = (num1 - num2).toString();
     }
-    
+
+    setResult(newResult);
+    setData([...data, { key: `${num1} ${operator} ${num2} = ${newResult}` }]);
   };
+
+  useEffect(() => {
+    setData([]);
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -48,8 +55,15 @@ export default function App() {
 
       <View style={styles.buttonContainer}>
         <Button onPress={() => handleCalculation('+')} title="+" />
-        <View style={styles.buttonSpace}/>
+        <View style={styles.buttonSpace} />
         <Button onPress={() => handleCalculation('-')} title="-" />
+      </View>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Text>{item.key}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -73,13 +87,19 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
   },
-  buttonSpace:
-  {
-    width:20
+  buttonSpace: {
+    width: 20,
   },
-  result:{
-    fontSize:30,
-    color:'#4682B4'
-  }
+  result: {
+    fontSize: 30,
+    color: '#4682B4',
+  },
+  listContainer: {
+    borderColor: 'black',
+    width: 200,
+    height: 100,
+    borderWidth: 1,
+    marginBottom: 10,
+    marginTop: 20,
+  },
 });
-
